e1173f5b98b6c1bca3876eb7522ea144959a0932
Naixes <615411375@qq.com>
2019-11-27 17:53:04 +0800

    map

---
+++ mylodash/map.js
@@ -0,0 +1,26 @@
+// 创建一个数组， value（值） 是 iteratee（迭代函数）遍历 collection（集合）中的每个元素后返回的结果。 iteratee（迭代函数）调用3个参数：(value, index|key, collection)
+function map(collection, iteratee) {
+  let index = -1
+  const length = collection == null ? 0 : collection.length
+  const result = new Array(length)
+
+  while (++index < length) {
+    result[index] = iteratee(collection[index], index, collection)
+  }
+  return result
+}
+
+function square(n) {
+  return n * n
+}
+
+console.log(map([4, 8], square))
+
+// 这个版本不支持
+
+// const users = [
+//     { 'user': 'barney' },
+//     { 'user': 'fred' }
+//   ]
+
+// console.log(map(users, 'user'))
