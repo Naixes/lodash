bf683ab950e9f72bd0a016a2db079a25c8f64ae3
Naixes <615411375@qq.com>
2019-11-27 09:46:55 +0800

    chunk

--- mylodash/chunk.js
+++ mylodash/chunk.js
@@ -1,33 +1,45 @@
-// function chunk(arr, size = 1) {
-//     let res = []
-//     let tem = []
-//     for(let i = 0; i < arr.length; i+=size) {
-//         tem = arr.slice(i, i + size)
-//         console.log('tem', tem)
-//         res.push(tem)
-//     }
-//     return res
-// }
+// 将数组按照指定长度切分为二维数组
+
+function _chunk(arr, size = 1) {
+  const res = []
+  let tem = []
+  for (let i = 0; i < arr.length; i+=size) {
+    tem = arr.slice(i, i + size)
+    console.log('tem', tem)
+    res.push(tem)
+  }
+  return res
+}
 
 import slice from '../slice.js'
 import toInteger from '../toInteger.js'
 
 function chunk(array, size = 1) {
-    size = Math.max(toInteger(size), 0)
-    console.log(size)
-    const length = array == null ? 0 : array.length
-    if (!length || size < 1) {
-      return []
-    }
-    let index = 0
-    let resIndex = 0
-    const result = new Array(Math.ceil(length / size))
-  
-    while (index < length) {
-      result[resIndex++] = slice(array, index, (index += size))
-    }
-    return result
+  // 处理参数
+  // size：可以是一组值，这里只取最大值
+  // **获取数组中的最大值：Math.max.apply(null,[10, 20,30]);/var max = Math.max(...arr);**
+  size = Math.max(toInteger(size), 0)
+  console.log('size', size)
+  // 获取长度
+  const length = array == null ? 0 : array.length
+  // 数组为空或size小于1返回空数组
+  if (!length || size < 1) {
+    return []
+  }
+  console.log('length', length)
+  // 原数组索引
+  let index = 0
+  // 新数组索引
+  let resIndex = 0
+  // 初始化新数组
+  const result = new Array(Math.ceil(length / size))
+
+  // 拼接新数组
+  while (index < length) {
+    result[resIndex++] = slice(array, index, (index += size))
+  }
+  return result
 }
 
 // export default chunk
-console.log(chunk([1,2,3,4,5,6,7], 2))
+console.log(chunk([1,2,3,4,5,6,7], (1,2,3)))
