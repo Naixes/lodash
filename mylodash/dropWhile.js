b3b341e060bc4d12c980a92e53f4fb3344c4bea4
Naixes <615411375@qq.com>
2019-11-28 14:59:29 +0800

    dropWhile

---
+++ mylodash/dropWhile.js
@@ -0,0 +1,42 @@
+console.log('==================baseWhile================')
+
+import slice from '../slice.js'
+
+// isDrop:是否去除
+function baseWhile(array, predicate, isDrop, fromRight) {
+  const { length } = array
+  let index = fromRight ? length : -1
+
+  console.log('index', index)
+
+  // 遇到第一个false就停止
+  while ((fromRight ? index-- : ++index < length) &&
+    predicate(array[index], index, array)) {}
+
+  console.log('index', index)
+
+  //   裁切
+  return isDrop
+    ? slice(array, (fromRight ? 0 : index), (fromRight ? index + 1 : length))
+    : slice(array, (fromRight ? index + 1 : 0), (fromRight ? length : index))
+}
+
+console.log('==================dropWhile================')
+
+// import baseWhile from '../.internal/baseWhile.js'
+
+// 创建一个切片数组，去除array中从起点开始到 predicate 返回假值结束部分。
+function dropWhile(array, predicate) {
+  return (array != null && array.length)
+    ? baseWhile(array, predicate, true)
+    : []
+}
+
+const users = [
+  { 'user': 'barney',  'active': true },
+  { 'user': 'fred',    'active': true },
+  { 'user': 'pebbles', 'active': false }
+]
+
+console.log(dropWhile(users, ({ active }) => active))
+// => objects for ['pebbles']
